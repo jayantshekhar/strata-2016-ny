6aea799960c55c0129a3103b173415a8944a9079
Jayant Shekhar <jayantshekhar@Jayants-iMac-2.local>
2016-09-14 00:46:33 -0700

    spam

--- src/main/scala/org/apache/spark/Spam.scala
+++ src/main/scala/org/apache/spark/Spam.scala
@@ -19,8 +19,10 @@
 package org.apache.spark
 
 // $example on$
+
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.{Tokenizer, StringIndexer}
+import org.apache.spark.ml.feature._
 import org.apache.spark.ml.recommendation.ALS
 import org.apache.spark.sql.types._
 
@@ -50,11 +52,61 @@ object Spam {
 
     ds.show(8)
 
+    // label
+    val indexer = new StringIndexer()
+      .setInputCol("spam")
+      .setOutputCol("label")
+    val indexed = indexer.fit(ds).transform(ds)
+
+    indexed.show()
+
+    // tokenize
     val tokenizer = new Tokenizer().setInputCol("message").setOutputCol("tokens")
+    val tokdata = tokenizer.transform(indexed)
 
-    val wordsData = tokenizer.transform(ds)
+    tokdata.show()
 
-    wordsData.show(8)
+    // tf
+    val hashingTF = new HashingTF()
+      .setInputCol("tokens").setOutputCol("tf").setNumFeatures(20)
+    val tfdata = hashingTF.transform(tokdata)
+
+    tfdata.show()
+
+    // idf
+    val idf = new IDF().setInputCol("tf").setOutputCol("idf")
+    val idfModel = idf.fit(tfdata)
+    val idfdata = idfModel.transform(tfdata)
+
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("idf"))
+      .setOutputCol("features")
+
+    val assemdata = assembler.transform(idfdata)
+
+    // split
+    val Array(trainingData, testData) = assemdata.randomSplit(Array(0.7, 0.3))
+
+    // lr
+    val lr = new LogisticRegression()
+      .setMaxIter(10)
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+      .setLabelCol("label")
+      .setFeaturesCol("features")
+
+
+    // Fit the model
+    val lrModel = lr.fit(assemdata)
+
+    val str = lrModel.toString()
+    println(str)
+
+    // predict
+    val predict = lrModel.transform(assemdata)
+
+
+    predict.show(100)
 
     spark.stop()
   }
