b9fe171831a0c53709f19059461a7525880535b7
Jayant Shekhar <jayantshekhar@Jayants-iMac-2.local>
2016-09-16 23:46:52 -0700

    graphx

--- src/main/scala/org/apache/spark/GraphXSample.scala
+++ src/main/scala/org/apache/spark/GraphXSample.scala
@@ -2,6 +2,7 @@ package org.apache.spark
 
 import org.apache.spark._
 import org.apache.spark.graphx._
+import org.apache.spark.graphx.util.GraphGenerators
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -29,7 +30,7 @@ object GraphXSample {
 
     // Create an RDD for edges
     val relationships: RDD[Edge[String]] =
-      sc.parallelize(Array(Edge(3L, 7L, "collab"),    Edge(5L, 3L, "advisor"),
+      sc.parallelize(Array(Edge(3L, 7L, "collab"), Edge(5L, 3L, "advisor"),
         Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
 
     // Define a default user in case there are relationship with missing user
@@ -39,7 +40,7 @@ object GraphXSample {
     val graph = Graph(users, relationships, defaultUser)
 
     // Count all users which are postdocs
-    var count = graph.vertices.filter { case (id, (name, pos)) => pos == "postdoc" }.count
+    var count = graph.vertices.filter { case (id, (name, pos)) => pos == "postdoc"}.count
     println("count is " + count)
 
     // Count all the edges where src > dst
@@ -79,6 +80,33 @@ object GraphXSample {
 
     // The subgraph operator takes vertex and edge predicates and returns the graph containing only the vertices that
     // satisfy the vertex predicate (evaluate to true) and edges that satisfy the edge predicate and connect vertices that satisfy the vertex predicate.
+
+
+    // pregel
+
+    pregel(sc)
+  }
+
+  def pregel(sc : SparkContext) {
+    val graph: Graph[Long, Double] =
+      GraphGenerators.logNormalGraph(sc, numVertices = 100).mapEdges(e => e.attr.toDouble)
+
+    val sourceId: VertexId = 42 // The ultimate source
+    // Initialize the graph such that all vertices except the root have distance infinity.
+    val initialGraph = graph.mapVertices((id, _) =>
+        if (id == sourceId) 0.0 else Double.PositiveInfinity)
+    val sssp = initialGraph.pregel(Double.PositiveInfinity)(
+      (id, dist, newDist) => math.min(dist, newDist), // Vertex Program
+      triplet => {  // Send Message
+        if (triplet.srcAttr + triplet.attr < triplet.dstAttr) {
+          Iterator((triplet.dstId, triplet.srcAttr + triplet.attr))
+        } else {
+          Iterator.empty
+        }
+      },
+      (a, b) => math.min(a, b) // Merge Message
+    )
+    println(sssp.vertices.collect.mkString("\n"))
 
 
   }
